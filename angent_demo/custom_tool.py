1506d957f6f76b3fb70b1de40d5a4d3c44086b06
bewit <bewithmeallmylife@163.com>
2024-03-19 16:12:15 +0800

    agent示例

---
+++ angent_demo/custom_tool.py
@@ -0,0 +1,32 @@
+from typing import Optional
+from math import sqrt, cos, sin
+
+desc = (
+    "use this tool when you need to calculate the length of a hypotenuse"
+    "given one or two sides of a triangle and/or an angle (in degrees). "
+    "To use the tool, you must provide at least two of the following parameters "
+    "['adjacent_side', 'opposite_side', 'angle']."
+)
+# 我们在这里需要多个输入，因为我们使用不同的值（边和角度）来计算三角形斜边。此外，并不需要 所有 值。我们可以使用任意两个或更多个参数来计算斜边。
+class PythagorasTool(BaseTool):
+    name = "Hypotenuse calculator"
+    description = desc
+
+    def _run(
+        self,
+        adjacent_side: Optional[Union[int, float]] = None,
+        opposite_side: Optional[Union[int, float]] = None,
+        angle: Optional[Union[int, float]] = None
+    ):
+        # check for the values we have been given
+        if adjacent_side and opposite_side:
+            return sqrt(float(adjacent_side)\*\*2 + float(opposite_side)\*\*2)
+            elif adjacent_side and angle:
+            return adjacent_side / cos(float(angle))
+        elif opposite_side and angle:
+            return opposite_side / sin(float(angle))
+        else:
+            return "Could not calculate the hypotenuse of the triangle. Need two or more of `adjacent_side`, `opposite_side`, or `angle`."
+
+    def _arun(self, query: str):
+        raise NotImplementedError("This tool does not support async")
